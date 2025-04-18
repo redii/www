85590b13d7fb5db8dd47547a3e78f11b231a7760
Henry Akmann <akmann.henry@gmail.com>
2025-04-18 13:03:38 +0200

    fixed bug with env variables

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,24 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/sveltekit/providers/github';
 
+import {
+	PRIVATE_AUTH_SECRET,
+	PRIVATE_AUTH_TRUST_HOST,
+	PRIVATE_AUTH_GITHUB_ID,
+	PRIVATE_AUTH_GITHUB_SECRET
+} from '$env/static/private';
+
 export const { handle, signIn, signOut } = SvelteKitAuth({
-	providers: [GitHub],
+	providers: [
+		GitHub({
+			clientId: PRIVATE_AUTH_GITHUB_ID,
+			clientSecret: PRIVATE_AUTH_GITHUB_SECRET
+		})
+	],
+	secret: PRIVATE_AUTH_SECRET,
+	trustHost: PRIVATE_AUTH_TRUST_HOST === 'true',
 	pages: {
 		signIn: '/login',
 		signOut: '/logout'
-	},
-	trustHost: true
+	}
 });
