2dbf42e68705791976930515c24ca166ace3c2ee
Henry Akmann <akmann.henry@gmail.com>
2025-06-12 08:48:47 +0200

    minor fix

--- src/lib/components/lightbox/LightboxItem.svelte
+++ src/lib/components/lightbox/LightboxItem.svelte
@@ -3,14 +3,12 @@
 	import { registerElement, openLightbox } from '$lib/components/lightbox/lightboxStore';
 
 	interface Props {
-		thumbnail?: string;
-		alt?: string;
 		gallery?: string;
 		children: Snippet;
 		lightboxContent?: Snippet;
 	}
 
-	let { thumbnail, alt, gallery = 'default', children, lightboxContent }: Props = $props();
+	let { gallery = 'default', children, lightboxContent }: Props = $props();
 	let elementIndex = 0 as number;
 
 	onMount(() => {
@@ -19,9 +17,7 @@
 </script>
 
 <button onclick={() => openLightbox(elementIndex, gallery)}>
-	{#if thumbnail}
-		<img src={thumbnail} {alt} />
-	{:else}
+	<div>
 		{@render children?.()}
-	{/if}
+	</div>
 </button>
--- src/lib/components/lightbox/LightboxOverlay.svelte
+++ src/lib/components/lightbox/LightboxOverlay.svelte
@@ -49,7 +49,7 @@
 
 				<Carousel.Content id="lightbox-carousel-content">
 					{#each $lightboxGalleries[$currentGalleryIndex].elements as element}
-						<Carousel.Item class="h-fit max-h-[75dvh] max-w-[100vw] sm:max-w-[90vw]">
+						<Carousel.Item class="max-h-[75dvh] max-w-[100vw] sm:max-w-[90vw]">
 							{@render element?.()}
 						</Carousel.Item>
 					{/each}
