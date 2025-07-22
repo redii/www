3649029fa94fcf635d575078739d58470990ccb0
Henry Akmann <akmann.henry@gmail.com>
2025-07-22 09:04:36 +0200

    minor change

--- src/routes/(public)/(auth)/login/+page.svelte
+++ src/routes/(public)/(auth)/login/+page.svelte
@@ -19,6 +19,6 @@
 
 <style>
 	:global(.signInButton button) {
-		@apply inline-flex h-11 w-full items-center justify-center gap-2 whitespace-nowrap rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0;
+		@apply inline-flex h-10 w-full items-center justify-center gap-2 rounded-md bg-primary px-8 text-sm font-medium whitespace-nowrap text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0;
 	}
 </style>
