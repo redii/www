353eaeba7096e7e7aa5d1f888aa580485ae29a37
akmann <akmann@proton.me>
2025-11-18 10:16:49 +0100

    minor change

--- src/routes/(public)/posts/+page.server.ts
+++ src/routes/(public)/posts/+page.server.ts
@@ -5,7 +5,8 @@ export const load: PageServerLoad = async () => {
 	const pageList = await readItems('posts', {
 		filter: import.meta.env.DEV
 			? { status: { _neq: 'archived' } }
-			: { status: { _eq: 'published' } }
+			: { status: { _eq: 'published' } },
+		sort: ['-date']
 	});
 
 	return { pageList };
